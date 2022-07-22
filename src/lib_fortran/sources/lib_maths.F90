995a725cf703c9f30b7dd353fcd47b6bdb19e065
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-07-22 15:13:17 +0200

    Now a fortran library can be built.

---
+++ src/lib_fortran/sources/lib_maths.F90
@@ -0,0 +1,13 @@
+module lib_maths
+    contains
+    pure subroutine vecAdd(n,a,b,c)
+        implicit none
+        integer(8), intent(in)  :: n
+        real(8)   , intent(in)  :: a(n), b(n)
+        real(8)   , intent(out) :: c(n)
+        integer(8)              :: i
+        do i = 1, n
+            c(i) = a(i) + b(i)
+        end do
+    end subroutine vecAdd
+end module lib_maths
