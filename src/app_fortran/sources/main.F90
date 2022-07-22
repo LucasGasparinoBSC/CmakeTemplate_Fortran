71516944875dfc58b0001405ccbef352cdfed422
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-07-22 16:54:35 +0200

    Made mpi optional in app_fortran.

--- src/app_fortran/sources/main.F90
+++ src/app_fortran/sources/main.F90
@@ -1,10 +1,21 @@
 program app_fortran
     use mod_output
     use lib_maths
+#ifndef NO_MPI
+    use mpi
+#endif
     implicit none
+    integer(4)                         :: ierr
+    integer(4)                         :: numRanks, rank
     integer(8), parameter              :: n = 10
     real(8), allocatable, dimension(:) :: a, b, c
     character(len=20)                  :: msg
+#ifndef NO_MPI
+    call mpi_init(ierr)
+    call mpi_comm_size(mpi_comm_world, numRanks, ierr)
+    call mpi_comm_rank(mpi_comm_world, rank, ierr)
+    print*, 'numRanks = ', numRanks, ' rank = ', rank
+#endif
     msg = 'Hello World'
     call Message(msg)
     allocate(a(n))
@@ -14,4 +25,10 @@ program app_fortran
     b(:) = 1.0d0
     call vecAdd(n,a,b,c)
     print*, c(1)
+    deallocate(a)
+    deallocate(b)
+    deallocate(c)
+#ifndef NO_MPI
+    call mpi_finalize(ierr)
+#endif
 end program app_fortran
