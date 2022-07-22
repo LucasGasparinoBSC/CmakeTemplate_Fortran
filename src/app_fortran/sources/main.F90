3f9528cb73e92b91e1dd9ef7ad6c25aecbd77d90
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-07-22 16:31:32 +0200

    libfortran now links to app_fortran.

--- src/app_fortran/sources/main.F90
+++ src/app_fortran/sources/main.F90
@@ -1,7 +1,17 @@
 program app_fortran
     use mod_output
+    use lib_maths
     implicit none
-    character(len=20) :: msg
+    integer(8), parameter              :: n = 10
+    real(8), allocatable, dimension(:) :: a, b, c
+    character(len=20)                  :: msg
     msg = 'Hello World'
     call Message(msg)
+    allocate(a(n))
+    allocate(b(n))
+    allocate(c(n))
+    a(:) = 1.0d0
+    b(:) = 1.0d0
+    call vecAdd(n,a,b,c)
+    print*, c(1)
 end program app_fortran
