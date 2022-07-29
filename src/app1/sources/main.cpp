87092a890d39124a45cb4aeb414c26dc536fa73b
Lucas Gasparino <lucas.gasparino@bsc.es>
2022-07-29 12:32:32 +0200

    Now metis is properly tested inside app1.

--- src/app1/sources/main.cpp
+++ src/app1/sources/main.cpp
@@ -83,6 +83,41 @@ int main()
     std::cout << "P1.sdim: " << P1.sdim << std::endl;
     #endif // !NO_SFC
 
+    #ifndef NO_METIS
+    // Testing wheter metis can be called
+    msg = "Using library function METIS...";
+    printMsg(msg);
+
+    // Define a mesh to be partitioned
+    idx_t nVertices = 6;
+    idx_t nEdges = 7;
+    idx_t nWeights = 1;
+    idx_t nParts = 2;
+    idx_t objval;
+    idx_t part[nVertices];
+
+    // Indexes of starting points in adjacent array
+    idx_t xadj[nVertices+1] = {0,2,5,7,9,12,14};
+
+    // Adjecent vertices inn consecutive index order
+    idx_t adjncy[2*nEdges] = {1,3,0,4,2,1,5,0,4,3,1,5,4,2};
+
+    // Weights of vertices
+    // If all weights are equal they can be set to null
+    idx_t vwgt[nVertices * nWeights];
+
+    int ret = METIS_PartGraphKway(&nVertices,& nWeights, xadj, adjncy,
+                                    NULL, NULL, NULL, &nParts, NULL,
+                                    NULL, NULL, &objval, part);
+
+    std::cout << ret << std::endl;
+
+    for(unsigned part_i = 0; part_i < nVertices; part_i++){
+	std::cout << part_i << " " << part[part_i] << std::endl;
+    }
+
+    #endif // !NO_METIS
+
     // Finalize MPI environment
     #ifndef NO_MPI
     mpiEnd();
